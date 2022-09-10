c4ad651e688fa0c76c1ef6ef51d01326755ec1db
Paola Rodrigues <paolarodrigues158@gmail.com>
2022-09-09 22:11:20 -0300

    Create a interpreter

--- chavesSharpBasic.py
+++ chavesSharpBasic.py
@@ -141,20 +141,41 @@ class Error:
         return result
 
 # Retorna todos os caracteres ilegais que não foram tratados
-
-
 class IllegalCharError(Error):
     def __init__(self, pos_start, pos_end, details):
-        super().__init__(pos_start, pos_end, 'Illegal Character', details)
+        super().__init__(pos_start, pos_end, 'Caracter invalido', details)
 
-
+# Retorna o erro de sintaxe
 class InvalidSyntaxError(Error):
     def __init__(self, pos_start, pos_end, details=''):
-        super().__init__(pos_start, pos_end, 'Invalid Syntax', details)
+        super().__init__(pos_start, pos_end, 'Sintaxe invalida', details)
+
+# Erros de execucao do interpretador 
+class RTError(Error):
+	def __init__(self, pos_start, pos_end, details, context):
+		super().__init__(pos_start, pos_end, 'Erro de execução', details)
+		self.context = context
+
+	def as_string(self):
+		result  = self.generate_traceback()
+		result += f'{self.error_name}: {self.details}'
+		result += '\n\n' + string_with_arrows(self.pos_start.ftxt, self.pos_start, self.pos_end)
+		return result
+
+	def generate_traceback(self):
+		result = ''
+		pos = self.pos_start
+		ctx = self.context
+
+		while ctx:
+			result = f'  File {pos.fn}, line {str(pos.ln + 1)}, in {ctx.display_name}\n' + result
+			pos = ctx.parent_entry_pos
+			ctx = ctx.parent
+
+		return 'Traceback (most recent call last):\n' + result
 
 ###################################### POSICAO ###############################################
 # Retorna a posicao
-
 
 class Position:
     def __init__(self, idx, ln, col, fn, ftxt):
@@ -185,6 +206,8 @@ class Position:
 class NumberNode:
     def __init__(self, tok):
         self.tok = tok
+        self.pos_start = self.tok.pos_start
+        self.pos_end = self.tok.pos_end
 
     # Retorna a representacao do token
     def __repr__(self):
@@ -197,6 +220,8 @@ class BinOpNode:
         self.left_node = left_node
         self.op_tok = op_tok
         self.right_node = right_node
+        self.pos_start = self.left_node.pos_start
+        self.pos_end = self.right_node.pos_end
 
     def __repr__(self):
         return f'({self.left_node}, {self.op_tok}, {self.right_node})'
@@ -206,13 +231,13 @@ class UnaryOpNode:
     def __init__(self, op_tok, node):
         self.op_tok = op_tok
         self.node = node
+        self.pos_start = self.op_tok.pos_start
+        self.pos_end = node.pos_end
 
     def __repr__(self):
         return f'({self.op_tok}, {self.node})'
 
 ############################################## PARSER ############################################
-
-
 class ParseResult:
     def __init__(self):
         self.error = None
@@ -256,7 +281,7 @@ class Parser:
         if not res.error and self.current_tok.type != TT_EOF:
             return res.failure(InvalidSyntaxError(
                 self.current_tok.pos_start, self.current_tok.pos_end,
-                "Expected 'mais', 'menos', 'vezes' or 'dividido'"
+                "Esperado 'mais', 'menos', 'vezes' ou 'dividido'"
             ))
         return res
 
@@ -287,7 +312,7 @@ class Parser:
             else:
                 return res.failure(InvalidSyntaxError(
                     self.current_tok.pos_start, self.current_tok.pos_end,
-                    "Expected ')'"
+                    "Esperado ')'"
                 ))
 
         return res.failure(InvalidSyntaxError(
@@ -319,6 +344,145 @@ class Parser:
 
         return res.success(left)
 
+######################################## INTERPRETADOR ###########################################
+# Vai retornar o resultado da sintaxe
+class Interpreter:
+	def visit(self, node, context):
+        # O metodo vai criar uma string chamada visit number node
+		method_name = f'visit_{type(node).__name__}'
+        # A partir do nome, vai ser chamado o metodo de acordo
+		method = getattr(self, method_name, self.no_visit_method)
+		return method(node, context)
+
+    # Mensagem para caso o metodo do no nao esteja definido
+	def no_visit_method(self, node, context):
+		raise Exception(f'No visit_{type(node).__name__} method defined')
+
+	############### TIPOS DE METODO QUE DEFINE OPERACOES ####################
+
+    # Retorna sucesso, pois não tem nenhuma operacao envolvida, é apenas numeros
+	def visit_NumberNode(self, node, context):
+		return RTResult().success(
+			Number(node.tok.value).set_context(context).set_pos(node.pos_start, node.pos_end)
+		)
+
+    # Esse metodo determina qual operacao vai ser executada
+	def visit_BinOpNode(self, node, context):
+		res = RTResult()
+		left = res.register(self.visit(node.left_node, context))
+		if res.error: return res
+		right = res.register(self.visit(node.right_node, context))
+		if res.error: return res
+
+		if node.op_tok.type == TT_PLUS:
+			result, error = left.added_to(right)
+		elif node.op_tok.type == TT_MINUS:
+			result, error = left.subbed_by(right)
+		elif node.op_tok.type == TT_MUL:
+			result, error = left.multed_by(right)
+		elif node.op_tok.type == TT_DIV:
+			result, error = left.dived_by(right)
+
+		if error:
+			return res.failure(error)
+		else:
+			return res.success(result.set_pos(node.pos_start, node.pos_end))
+
+    # Verica operacoes unitarias, como por exemplo -4 ...
+	def visit_UnaryOpNode(self, node, context):
+		res = RTResult()
+		number = res.register(self.visit(node.node, context))
+		if res.error: return res
+
+		error = None
+
+        # Caso o operador e menos, multiplica por menos 1 para nega-lo
+		if node.op_tok.type == TT_MINUS:
+			number, error = number.multed_by(Number(-1))
+
+		if error:
+			return res.failure(error)
+		else:
+			return res.success(number.set_pos(node.pos_start, node.pos_end))
+
+################################### ARMAZENANDO VALORES #####################################
+# Essa classe serve apenas para armazenas os valores e em  seguida opera-los com outros numeros
+
+class Number:
+	def __init__(self, value):
+		self.value = value
+		self.set_pos()
+		self.set_context()
+
+    # Gravando a posicao para que caso tenha alguma divisao por 0 o erro vai ser exibido corretamente
+	def set_pos(self, pos_start=None, pos_end=None):
+		self.pos_start = pos_start
+		self.pos_end = pos_end
+		return self
+
+	def set_context(self, context=None):
+		self.context = context
+		return self
+
+    # As operacoes abaixo fazem a validacao se esta sendo feitas as operacoes com outros numeros, ou seja, pegando outra posicao
+    # Todos retornan um numero e adicionam ao um outro valor
+	def added_to(self, other):
+		if isinstance(other, Number):
+			return Number(self.value + other.value).set_context(self.context), None
+
+	def subbed_by(self, other):
+		if isinstance(other, Number):
+			return Number(self.value - other.value).set_context(self.context), None
+
+	def multed_by(self, other):
+		if isinstance(other, Number):
+			return Number(self.value * other.value).set_context(self.context), None
+
+	def dived_by(self, other):
+		if isinstance(other, Number):
+			if other.value == 0:
+				return None, RTError(
+					other.pos_start, other.pos_end,
+					'Divisão por 0',
+					self.context
+				)
+
+			return Number(self.value / other.value).set_context(self.context), None
+
+	def __repr__(self):
+		return str(self.value)
+
+####################################### RESULTADOS DE EXECUCAO ##############################################
+# Essa classe acompanha o resultado atual e o erro 
+class RTResult:
+	def __init__(self):
+		self.value = None
+		self.error = None
+
+    # Veridica se ocorreu algum erro, se sim retorna o erro
+	def register(self, res):
+		if res.error: self.error = res.error
+		return res.value
+
+    # Sucesso atualiza o valor 
+	def success(self, value):
+		self.value = value
+		return self
+
+    # A parte de falha atualiza o erro para a mensagem
+	def failure(self, error):
+		self.error = error
+		return self
+
+
+######################################## CONTEXTO ERRO ########################################
+# Atualiza o erro em tempo de execucao e mostra o contexto do erro
+class Context:
+	def __init__(self, display_name, parent=None, parent_entry_pos=None):
+		self.display_name = display_name
+		self.parent = parent
+		self.parent_entry_pos = parent_entry_pos
+
 ######################################### RUN ####################################################
 
 # A funcao abaixo vai pegar o texto e executar no terminal
@@ -334,6 +498,13 @@ def run(fn, text):
     # Gerando a sintaxe abstrata
     parser = Parser(tokens)
     ast = parser.parse()
+    if ast.error: return None, ast.error
+
+    # Executando o programa ja com os resultados
+    interpreter = Interpreter()
+    context = Context('<program>')
+    result = interpreter.visit(ast.node, context)
 
     # Remove o no da arvore e tambem remove o erro
-    return ast.node, ast.error
+    # return ast.node, ast.error
+    return result.value, result.error
